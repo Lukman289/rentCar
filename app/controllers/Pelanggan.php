020ec0e84643233ec053c354d0b6b0a3154eb3dd
Muhammad Khairunnas Nurdin <anasnurdin@gmail.com>
2023-12-08 22:59:32 +0700

    finishing code to have return void

--- app/controllers/Pelanggan.php
+++ app/controllers/Pelanggan.php
@@ -6,7 +6,7 @@ use core\Controller;
 
 class Pelanggan extends Controller
 {
-	public function index()
+	public function index(): void
 	{
 		$data['title'] = "Pelanggan";
 		$data['style'] = "landingpage";
