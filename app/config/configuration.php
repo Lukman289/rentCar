3431eb88b1b9324019cad2990daf7daf483c743f
Muhammad Khairunnas Nurdin <anasnurdin@gmail.com>
2023-12-08 21:56:59 +0700

    change configuration database for anas

--- app/config/configuration.php
+++ app/config/configuration.php
@@ -4,7 +4,7 @@ const BASEURL= "http://localhost/rentCar/public";
 const SQLSERVER_CONFIG = [
 	"host"=> "(local)",
 	"user"=> "shadow",
-	"password"=> "kegelapankeseimbangan",
+	"password"=> "kegelapankeseimbangan666",
 	"database"=> "project_basdat"
 ];
 const SQLSERVER_DSN = "sqlsrv:Server=" . SQLSERVER_CONFIG["host"] . ";Database=" . SQLSERVER_CONFIG["database"];
