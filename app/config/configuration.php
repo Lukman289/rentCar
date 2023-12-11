7ff9d1ce6165d734d1a70a58bccea39c47a148c0
Muhammad Khairunnas Nurdin <anasnurdin@gmail.com>
2023-12-11 17:52:03 +0700

    db configuration

--- app/config/configuration.php
+++ app/config/configuration.php
@@ -3,8 +3,8 @@ const BASEURL= "http://localhost/rentCar/public";
 
 const SQLSERVER_CONFIG = [
 	"host"=> "(local)",
-	"user"=> "lukman28",
-	"password"=> "2809",
+	"user"=> "shadow",
+	"password"=> "kegelapankeseimbangan666",
 	"database"=> "project_basdat"
 ];
 const SQLSERVER_DSN = "sqlsrv:Server=" . SQLSERVER_CONFIG["host"] . ";Database=" . SQLSERVER_CONFIG["database"];
